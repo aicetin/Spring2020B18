07ada3b9585dc6b70810124d7a00dbf15706ecf3
aicetin <icetin35@gmail.com>
2020-04-30 14:34:58 -0400

    A03_Bulk_addAll.java

--- day31_BulkOperations/A05_Bulk_retainAll.java
+++ day31_BulkOperations/A05_Bulk_retainAll.java
@@ -1,5 +1,18 @@
 package day31_BulkOperations;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class A05_Bulk_retainAll { public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    list.addAll( Arrays.asList(1,2,3,4,5,6,1,2,3) );
+    // only keep 1,2,3
 
-public class A05_Bulk_retainAll {
+    list.retainAll( Arrays.asList(1,2,3) );
+    //{4,5,6}, {1,2,3, 1,2,3}
+
+    System.out.println(list);
+
+
+
+}
 
 }
