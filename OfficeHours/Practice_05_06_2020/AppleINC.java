83c15be107cf08273b9be8f844147242d68e15af
aicetin <icetin35@gmail.com>
2020-05-06 17:32:26 -0400

    Office Class

--- OfficeHours/Practice_05_06_2020/AppleINC.java
+++ OfficeHours/Practice_05_06_2020/AppleINC.java
@@ -1,4 +1,72 @@
 package OfficeHours.Practice_05_06_2020;
 
 public class AppleINC {
+
+    public static void main(String[] args) {
+        Tester tester1 = new Tester();
+        tester1.setInfo("Onur", "Junior Tester", 75000, 12334);
+
+        Tester tester2 = new Tester();
+        tester2.setInfo("Ibrahim", "Junior Tester", 90000, 13425);
+
+        Tester tester3 = new Tester();
+        tester3.setInfo("Ali", "SDET", 80000, 6575);
+
+        ScrumTeam scrum1 = new ScrumTeam(); // we can use the arraylist in scrumTeam class
+        scrum1.hireATester(tester2);
+        scrum1.hireATester(tester3);
+        scrum1.hireATester(tester1);
+
+        scrum1.fireATester(12334);
+
+        // scrum1.testersTeam // returns the testersTeam arraylist from scrum1
+
+        for( Tester each  : scrum1.testersTeam){  // prints out every single tester from scrumTeam
+            System.out.println(each);
+        }
+        System.out.println("=============================================");
+        Developer dev1 = new Developer();
+        dev1.setInfo("Barzy", "Senior Developer", 110000, 54546);
+
+        Developer dev2 = new Developer();
+        dev2.setInfo("Oguz", "Usta Developer", 120000, 64646);
+
+        scrum1.hireADeveloper(dev1);
+        scrum1.hireADeveloper(dev2);
+
+        scrum1.fireADeveloper(64646);
+
+
+
+        System.out.println(scrum1.devpTeam.size());
+
+        for(Developer each : scrum1.devpTeam){ // arraylist of developer in scrumTeam
+            System.out.println(each);
+
+            System.out.println("======================================");
+            System.out.println("In my scrum Team, we have "+scrum1.testersTeam.size()+" testers and "+scrum1.devpTeam.size()+" developers");
+    }
+
+        /*
+        create scrum2: 4 developers, 2 testers
+        create scrum3: 5 developers, 3 testers
+
+        create an array of scrum: {scrum1, scrum2,scrum3};
+        1. iterator the array to print out all the testers who have salary > 120000
+        2. print out all the developers who have salary < 120000
+
+        create an Arraylist of scrum: {scrum1, scrum2, scrum3}
+        1. remove all the testers who have salary < 100000
+        2. remove all the testers who have salary < 120000
+
+
+           NOTE: DO NOT create any ArrayList of Testers or Developers.
+                Use the ArrayLists in ScrumTeam class only
+
+         */
+
+
+
+
+}
 }
--- OfficeHours/Practice_05_06_2020/DevelopersObject.java
+++ OfficeHours/Practice_05_06_2020/DevelopersObject.java
@@ -1,4 +1,70 @@
 package OfficeHours.Practice_05_06_2020;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DevelopersObject {
+
+    public static void main(String[] args) {
+        Developer developer1 = new Developer();
+        System.out.println(developer1);
+        developer1.setInfo("Mehmet", "Senior Developer", 160_000, 12335663 );
+        System.out.println(developer1);
+
+        Developer developer2 = new Developer();
+        developer2.setInfo("Leon", "Senior Developer", 110_000, 14151516);
+
+        developer1.coding();
+        developer2.coding();
+
+        developer1.fixingBug();
+        developer2.fixingBug();
+
+        System.out.println("=====================================================================");
+
+        Developer[] arr = {developer1, developer2};
+        /*
+        for(int i = 0; i<arr.length; i++){
+
+            System.out.println(arr[i]);
+
+         */
+        for( Developer each : arr){
+            System.out.println(each);
+            //System.out.println(each.name+ " "+each.salary);
+
+            System.out.println("=================================================");
+
+        ArrayList<Developer> developers = new ArrayList<>(Arrays.asList(arr));
+            // developers.addAll(Arrays.asList(developer1,developer2));
+
+        System.out.println(developers.size());
+            /*
+            for(int i = 0; i< developers.size(); i++){
+                System.out.println(developers.get(i).name);
+
+            */
+
+        for( Developer each1: developers){
+
+                System.out.println(each1);
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
 }
