ef5bedbbd419837e7f217d739dacbd37cee82027
aicetin <icetin35@gmail.com>
2020-05-15 16:39:40 -0400

    day40 Class

---
+++ day13_StringClass/WUT2_ShippingInfo_WUT.java
@@ -0,0 +1,17 @@
+package day13_StringClass;
+/*
+2. write a program for the shipping info that, the program should ask:
+					building number
+					Street address (Assume it has more than one word)
+					city name
+					state name
+					zip code
+					full name of the person:
+			and prints the ship to info in the following format:
+			ex output:
+					Ship To:  Jimmy joe
+							  7925 Jones Branch Dr
+							  MCLean, VA 22102
+ */
+public class WUT2_ShippingInfo_WUT {
+}
