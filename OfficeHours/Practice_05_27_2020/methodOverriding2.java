df0e1b54c65bf138b05301345bfeff8edaf77677
aicetin <icetin35@gmail.com>
2020-05-28 13:35:23 -0400

    Practice_05_27_2020

--- OfficeHours/Practice_05_27_2020/methodOverriding2.java
+++
@@ -1,4 +0,0 @@
-package OfficeHours.Practice_05_27_2020;
-
-public class methodOverriding2 {
-}
---
+++ day39_AccessModifiers/SummaryOf_AccessModifiers.java
@@ -0,0 +1,12 @@
+package day39_AccessModifiers;
+
+public class SummaryOf_AccessModifiers {
+    /*
+    Summary of Access Modifiers: public > protected > default > private
+• we can call public variables and methods any where in any class in one whole project.
+• we can call protected variables and methods in any class in one package.
+• we can call protected variables and methods in any sub class in one whole project.
+• we can call default variables and methods in any class in one package ONLY.
+• we can call private variables and methods inside one class ONLY.
+     */
+}
